de32055a6945cee5c2fbff53679d7d0e505f1478
Konstantīns Kotovičs <kostja.kotovich@gmail.com>
2026-03-03 22:23:44 +0200

    ievades dati un virknes ģenerēšana

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import random
+
+start_array_length = input("Lūdzu ievadiet virknes garumu (no 15 līdz 25): ")
+try:
+    start_array_length = int(start_array_length)
+except:
+    print("Nepareiza ievade, lūdzu, ievadiet veselo skaitli.")
+
+number_list = []
+for i in range(0, start_array_length):
+    number_list.append(random.randint(1, 9))
+
+def print_list():
+    for i in range(0, len(number_list)):
+        print(number_list[i], end=" ")
+
+print_list()
